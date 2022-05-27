57eac575e715e9c5e0dce9180daf0c3f5c41787c
Robert Lieck <robert.lieck@durham.ac.uk>
2022-05-27 17:10:03 +0200

    update documentation and tests

---
+++ tests/test_examples.py
@@ -0,0 +1,25 @@
+#  Copyright (c) 2022 Robert Lieck.
+from unittest import TestCase
+
+import os
+import importlib.util
+
+
+class TestExamples(TestCase):
+    def test_examples(self):
+        """Run all the examples from the documentation."""
+        examples_dir = 'examples'
+        self.assertTrue(os.path.isdir(examples_dir))
+        os.chdir(examples_dir)
+        for dir_path, dir_names, file_names in os.walk("."):
+            for file in file_names:
+                full_path = os.path.join(dir_path, file)
+                if not file.endswith(".py"):
+                    continue
+                print(f"loading: {file}")
+                # sourcing directly hides problematic lines in error traces
+                # exec(open(os.path.join(dir_path, file)).read())
+                # import as module instead
+                spec = importlib.util.spec_from_file_location("", full_path)
+                mod = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(mod)
