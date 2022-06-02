e2ca88d75a27a6cef778d4995f06a5c216563f14
Robert Lieck <robert.lieck@durham.ac.uk>
2022-06-02 22:51:06 +0100

    version 0.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="musicflower",
-    version="0.0.3",
+    version="0.0.4",
     author="Robert Lieck",
     author_email="robert.lieck@durham.ac.uk",
     description="",
