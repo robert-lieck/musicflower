e59c1b2ad03b9bc40eb71dd8084d7cec1fdab5c4
Robert Lieck <robert.lieck@durham.ac.uk>
2025-05-19 18:05:18 +0200

    update main script

--- musicflower/__main__.py
+++ musicflower/__main__.py
@@ -1,26 +1,14 @@
-from musicflower.webapp import WebApp, fourier_visualiser, keyscape, tonnetz_visualiser, spectral_dome
+from musicflower.webapp import WebApp
+import musicflower.features as f
+import musicflower.visualisers as v
 
 if __name__ == '__main__':
-
-    app = WebApp(verbose=True)  # print additional info registered features, callbacks etc.
-    app.use_chroma_features(200)  # max resolution of 200
-    app.use_fourier_features()
-    app.use_chroma_scape_features()
-    app.use_fourier_scape_features()
-    app.register_visualiser('Fourier Coefficients',
-                            ['fourier-features'],
-                            fourier_visualiser)
-    app.register_visualiser('Keyscape',
-                            ['fourier-scape-features',
-                             'chroma-scape-features'],
-                            keyscape)
-    app.register_visualiser('Tonnetz',
-                            ['chroma-features'],
-                            tonnetz_visualiser)
-    app.register_visualiser('Spectral Dome',
-                            ['fourier-scape-features',
-                             'chroma-scape-features'],
-                            spectral_dome)
+    app = WebApp(verbose=True)         # print additional info about registered features, callbacks etc.
+    f.use_chroma_features(app, n=200)  # max temporal resolution of 200
+    v.use_fourier_visualiser(app)      # register visualiser and required features
+    v.use_keyscape_visualiser(app)
+    v.use_tonnetz_visualiser(app)
+    v.use_spectral_dome_visualiser(app)
     app.init(
         figure_width=1500,
         figure_height=800,
